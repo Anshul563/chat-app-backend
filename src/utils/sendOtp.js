e273b3788690f9278f7be801eb3ae3c423df7933
Anshul563 <anshulshakya18168@gmail.com>
2025-12-30 14:35:09 +0530

    otp

--- src/utils/sendOtp.js
+++ src/utils/sendOtp.js
@@ -1,4 +1,6 @@
 import SibApiV3Sdk from "sib-api-v3-sdk";
+import dotenv from "dotenv";
+dotenv.config();
 
 const client = SibApiV3Sdk.ApiClient.instance;
 client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
